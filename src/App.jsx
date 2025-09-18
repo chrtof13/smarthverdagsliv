5325fe0d0be57850e6a6794992d70d1681e1e577
christoffertoft13 <chrtof13@gmail.com>
2025-09-18 19:10:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
-import RedirectArticleById from "./pages/RedirectArticleById"; // lager vi straks
+// src/App.jsx
+import { Routes, Route } from "react-router-dom";
 
-<Routes>
-    <Route path="/" element={<Home />} />
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Privacy from "./pages/Privacy";
+import ArticlePage from "./pages/ArticlePage";
+import RedirectArticleById from "./pages/RedirectArticleById";
 
-    {/* NY: slug-basert URL */}
-    <Route path="/artikler/:slug" element={<ArticlePage />} />
+import Header from "./components/Header";
+import ScrollToTop from "./components/ScrollToTop";
 
-    {/* GAMMEL sti -> redirect */}
-    <Route path="/article/:id" element={<RedirectArticleById />} />
+import "./App.css";
 
-    <Route path="/about" element={<About />} />
-    <Route path="/contact" element={<Contact />} />
-    <Route path="/privacy" element={<Privacy />} />
-    <Route path="*" element={<Home />} />
-</Routes>
+export default function App() {
+    return (
+        <div className="min-h-screen bg-gray-50 text-gray-900">
+            <ScrollToTop />
+            <Header />
+
+            <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 py-8">
+                <Routes>
+                    {/* Forside */}
+                    <Route path="/" element={<Home />} />
+
+                    {/* Nye, pene URLer med slug */}
+                    <Route path="/artikler/:slug" element={<ArticlePage />} />
+
+                    {/* Gamle ID-URLer -> redirect til slug */}
+                    <Route path="/article/:id" element={<RedirectArticleById />} />
+
+                    {/* Statiske sider */}
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/privacy" element={<Privacy />} />
+
+                    {/* Fallback */}
+                    <Route path="*" element={<Home />} />
+                </Routes>
+            </div>
+        </div>
+    );
+}
